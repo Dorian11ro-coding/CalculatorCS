0ee215c61845895e884d55aa21360f2d5a1271ac
Huk <minimickey427@yahoo.com>
2024-10-27 20:04:38 +0200

    Calculator in C#

---
+++ CalculatorCS/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("For additions, press 1");
+Console.WriteLine("For subtractions, press 2");
+Console.WriteLine("For multiplications, press 3");
+Console.WriteLine("For divisions, press 4");
+
+int choice = int.Parse(Console.ReadLine());
+
+if (choice == 1)
+{
+    Console.WriteLine("Num 1: ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Num 2: ");
+    int num2 = int.Parse(Console.ReadLine());
+    int sum = num1 + num2;
+    Console.WriteLine("Addition is " + sum);
+
+}
+
+if (choice == 2)
+{
+    Console.WriteLine("Num 1: ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Num 2: ");
+    int num2 = int.Parse(Console.ReadLine());
+    int sum = num1 - num2;
+    Console.WriteLine("Subtraction is " + sum);
+}
+
+if (choice == 3)
+{
+    Console.WriteLine("Num 1: ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Num 2: ");
+    int num2 = int.Parse(Console.ReadLine());
+    int sum = num1 * num2;
+    Console.WriteLine("Multiplication is " + sum);
+}
+
+if (choice == 4)
+{
+    Console.WriteLine("Num 1: ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Num 2: ");
+    int num2 = int.Parse(Console.ReadLine());
+    int sum = num1 / num2;
+    Console.WriteLine("Division is " + sum);
+}
